10f1431bb5a9e3e808432ca77c0f92656e073f0f
Hritik-del <74361880+Hritik-del@users.noreply.github.com>
2023-07-26 21:13:53 +0530

    Added code for who wins

--- javascript.js
+++ javascript.js
@@ -10,6 +10,17 @@ var randomNumber2 = Math.floor(Math.random()*6) + 1;
 
 randomImageName = "images/dice" + randomNumber2 + ".png";
 
-img1 = document.querySelectorAll("img")[1];
+var img2 = document.querySelectorAll("img")[1];
 
-img1.setAttribute("src", randomImageName);
+img2.setAttribute("src", randomImageName);
+
+var whoWins = document.querySelector("h1");
+if(randomNumber1 > randomNumber2){
+    whoWins.innerHTML = "🚩Player 1 Wins!"
+}
+else if(randomNumber1 < randomNumber2){
+    whoWins.innerHTML = "Player 2 Wins!🚩"
+}
+else{
+    whoWins.innerHTML = "Draw!📍"
+}
